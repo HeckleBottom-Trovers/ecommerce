c80f102496b2942f85f4b49e634474a509b0ca4b
abhipaid <abhi.paid301@gmail.com>
2025-05-24 12:42:44 -0700

    admin progress

--- src/main/java/com/HecklebottomTrovers/ecommerce/EcommerceApplication.java
+++ src/main/java/com/HecklebottomTrovers/ecommerce/EcommerceApplication.java
@@ -1,14 +1,31 @@
 package com.HecklebottomTrovers.ecommerce;
 
+import com.HecklebottomTrovers.ecommerce.User;
+import com.HecklebottomTrovers.ecommerce.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class EcommerceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(EcommerceApplication.class, args);
-    }
-}
 
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner createAdminAccount(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			if (userRepository.findByUsername("admin") == null) {
+				User admin = new User();
+				admin.setUsername("admin");
+				admin.setPassword(passwordEncoder.encode("adminpass"));
+				userRepository.save(admin);
+				System.out.println("Admin account created.");
+			}
+		};
+	}
+}
